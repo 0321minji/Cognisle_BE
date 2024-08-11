37b037f74f0c3a06dd9661666b360031dc7795e2
밍 <79096808+0321minji@users.noreply.github.com>
2024-08-11 16:31:41 +0900

    #64 fix : add logging for django in ec2 (#65)

--- Cognisle/settings/production.py
+++ Cognisle/settings/production.py
@@ -4,3 +4,21 @@ DEBUG = False
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 SECURE_SSL_REDIRECT = True
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/home/ubuntu/Cognisle_BE/logs/errors.log',  # 로그 파일 경로
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
